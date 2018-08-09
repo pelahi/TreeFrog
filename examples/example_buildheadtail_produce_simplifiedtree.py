6b051d07bafc3f6f1c4dbfaf9bbddbdb6a161dde
pelahi <pascaljelahi@gmail.com>
2018-08-09 15:44:24 +0800

    Added examples of manipulating TreeFrog output

---
+++ examples/example_buildheadtail_produce_simplifiedtree.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 11:05:33 2017
+
+Example of loading a raw tree, building the head/tail, root head/ root tail info and fixing the code for branch swapping events and writing a simplified HDF tree. 
+@author: Pascal Jahan Elahi
+
+
+"""
+
+import sys
+import os
+import psutil
+import time
+import numpy as np
+import copy
+
+#load python routines
+sys.path.append(sys.argv[1])
+import velociraptor_python_tools as vpt
+
+#base raw tree file name to load the raw tree if necessary
+basetreefname=sys.argv[2]
+
+#base halo properties 
+basepropfname=sys.argv[3]
+
+#number of snapshots
+numsnaps=int(sys.argv[4])
+
+#file name for the simplified tree file
+outputfname=sys.argv[5]
+
+#define properties of interest
+requestedfields=[
+    'ID', 'hostHaloID', 
+    'npart', 
+    'Xc', 'Yc', 'Zc', 
+    'VXc', 'VYc', 'VZc', 
+    'Rmax', 'Vmax', 
+    'Structuretype'
+    ]
+
+#define different types of input
+ASCIIINPUT=0
+HDFINPUT=2
+
+#could alter to have user indicate input type but currently assume all is HDF
+RAWTREEFORMAT=HDFINPUT
+RAWPROPFORMAT=HDFINPUT
+TEMPORALHALOIDVAL=1000000000000
+#number of snapshots searched when producing the tree
+NSNAPSEARCH=4
+
+#read raw descendant tree along with merits, don't reverse snap order, 
+
+if (RAWTREEFORMAT == HDFINPUT):
+    #for hdf input produce file listing input files 
+    snaptreelist=open(basetreefname+'.snaptreelist.txt','w')
+    for i in range(numsnaps):
+        snaptreelist.write(basetreefname+'.snapshot_%03d.VELOCIraptor\n'%i)
+    snaptreelist.close()
+    rawtreedata=vpt.ReadHaloMergerTreeDescendant(basetreefname+'.snaptreelist.txt', False, RAWTREEFORMAT, 1, True)
+else:
+    rawtreedata=vpt.ReadHaloMergerTreeDescendant(basetreefname, False, RAWTREEFORMAT, 0, True)
+print('Finished reading raw tree')
+numhalos=np.zeros(numsnaps,dtype=np.uint64)
+halodata=[dict() for i in range(numsnaps)]
+atime=np.zeros(numsnaps)
+for i in range(numsnaps):
+    halodata[i],numhalos[i]=vpt.ReadPropertyFile(basepropfname+'%03d.VELOCIraptor'%i, 2, 0, 1, requestedfields)
+    atime[i]=halodata[i]['SimulationInfo']['ScaleFactor']
+    for key in halodata[i].keys():
+        if (key == 'SimulationInfo' or key == 'UnitInfo'): continue
+        if (halodata[i][key].dtype==np.float64):
+            halodata[i][key] = np.array(halodata[i][key],dtype=np.float32)
+print('Finished reading halo properties')
+
+#produce head tail in ascending order
+start=time.clock()
+print("Building head/tail ")
+vpt.BuildTemporalHeadTailDescendant(numsnaps,rawtreedata,numhalos,halodata,TEMPORALHALOIDVAL)
+print("Finished head/tail ", time.clock()-start)
+
+start=time.clock()
+print('Fixing branch swapping events')
+#set the limits to use when trying to fix branch swapping events.
+npartlim=200
+meritlim=0.025
+xdifflim=2.0
+vdifflim=1.0
+vpt.FixTruncationBranchSwapsInTreeDescendant(numsnaps, rawtreedata, halodata, numhalos, npartlim, meritlim, xdifflim, vdifflim, NSNAPSEARCH, TEMPORALHALOIDVAL)
+print("Finished fixing branch swapping events", time.clock()-start)
+
+#write the tree and quit
+DescriptionInfo={
+        'Title':'Walkable Tree', 'TreeBuilder':'TreeFrog', 'TreeBuilder_version':1.2, 'Temporal_linking_length':NSNAPSEARCH, 'Temporal_halo_id_value':TEMPORALHALOIDVAL}
+vpt.WriteWalkableHDFTree(outputfname,numsnaps,rawtreedata,numhalos,halodata,atime,DescriptionInfo)
+quit()
+
---
+++ examples/example_produce_unified_file.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 11:05:33 2017
+
+Example of loading a tree, associated halo properties and building sublinks, progenitor links 
+@author: Pascal Jahan Elahi
+
+
+"""
+
+import sys
+import os
+import psutil
+import time
+import numpy as np
+import copy
+
+#load python routines
+sys.path.append(sys.argv[1])
+import velociraptor_python_tools as vpt
+
+#load the hdf tree file
+inputhdftreefname=sys.argv[2]
+
+#base halo properties 
+basepropfname=sys.argv[3]
+
+#file name for the unified file 
+outputfname=sys.argv[4]
+
+#define properties of interest
+requestedfields=[
+    'ID', 'hostHaloID', 
+    'numSubStruct', 'npart', 
+    'Mass_tot', 'Mass_FOF', 'Mass_200mean', 'Mass_200crit', 
+    'R_size', 'R_HalfMass', 'R_200mean', 'R_200crit', 
+    'Xc', 'Yc', 'Zc', 
+    'VXc', 'VYc', 'VZc', 
+    'lambda_B',
+    'Lx','Ly','Lz',
+    'RVmax_Lx','RVmax_Ly','RVmax_Lz',
+    'sigV', 'RVmax_sigV',
+    'Rmax', 'Vmax', 
+    'cNFW', 
+    'Efrac','Structuretype'
+    ]
+
+#define different types of input
+ASCIIINPUT=0
+HDFINPUT=2
+
+#could alter to have user indicate input type but currently assume all is HDF
+RAWTREEFORMAT=HDFINPUT
+RAWPROPFORMAT=HDFINPUT
+TEMPORALHALOIDVAL=1000000000000
+#number of snapshots searched when producing the tree
+NSNAPSEARCH=4
+
+
+#load walkable tree information.
+if (os.path.exists(inputhdftreefname)):
+    rawtreedata=[]
+    halodata,numsnaps=vpt.ReadWalkableHDFTree(inputhdftreefname)
+    #if loading walkable tree, assume tree processed for branch swapping events and need to update the halodata dictionary
+    numhalos=np.zeros(numsnaps,dtype=np.int64)
+    atime=np.zeros(numsnaps)
+    print(numsnaps)
+    #load halo properties file 
+    for i in range(numsnaps):
+        fname=basepropfname+'%03d.VELOCIraptor'%i
+        print(fname)
+        halos,numhalos[i] = vpt.ReadPropertyFile(fname,RAWPROPFORMAT,0,0,requestedfields)
+        atime[i]=halos['SimulationInfo']['ScaleFactor']
+        halodata[i].update(halos)
+        for key in halodata[i].keys():
+            if (key == 'SimulationInfo' or key == 'UnitInfo'): continue
+            if (halodata[i][key].dtype==np.float64):
+                halodata[i][key] = np.array(halodata[i][key],dtype=np.float32)
+else:
+    print("error, file not found ")
+    exit()
+
+#generate subhalo links
+vpt.GenerateSubhaloLinks(numsnaps,numhalos,halodata)
+#generate progenitor links
+vpt.GenerateProgenitorLinks(numsnaps,numhalos,halodata)
+
+#strip out simulation and unit data
+SimulationInfo=copy.deepcopy(halodata[0]['SimulationInfo'])
+UnitInfo=copy.deepcopy(halodata[0]['UnitInfo'])
+if SimulationInfo['Cosmological_Sim']:
+    if not UnitInfo['Comoving_or_Physical']: 
+        #convert period to comoving little h
+        SimulationInfo['Period']*=SimulationInfo['h_val']/SimulationInfo['ScaleFactor']
+    del SimulationInfo['ScaleFactor']
+    #lets update the names for genesis style
+    UnitInfo['Comoving_unit_flag'] = UnitInfo['Comoving_or_Physical']
+
+#remove the unit info from each snapshot as it is unnecessary
+for i in range(numsnaps):
+    del halodata[i]['SimulationInfo']
+    del halodata[i]['UnitInfo']
+
+#currently only dark matter runs
+igas=istar=ibh=0
+#write the unified file that contains forest ids, properties, 
+#description will have to be updated so as to use appropriate version numbers
+DescriptionInfo={
+        'Title':'Tree and Halo', 'HaloFinder':'VELOCIraptor', 'TreeBuilder':'TreeFrog', 
+        'HaloFinder_version':1.25, 'TreeBuilder_version':1.2, 
+        'Particle_num_threshold':20, 'Temporal_linking_length':4, 'Temporal_halo_id_value':TEMPORALHALOIDVAL,
+        'Flag_gas':(igas==1), 'Flag_star':(istar==1), 'Flag_bh':(ibh==1),
+        'Flag_subhalo_links':True, 'Flag_progenitor_links':True, 'Flag_forest_ids':False
+        }
+vpt.WriteUnifiedTreeandHaloCatalog(outputfname, numsnaps, rawtreedata, numhalos, halodata, atime, 
+                                   DescriptionInfo,SimulationInfo,UnitInfo)
+
+
